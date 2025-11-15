f89a456f23693377a1c054d93763ea136e706014
mamunrakib70 <mamunrakib70@gmail.com>
2025-11-15 21:14:08 +0600

    hash added

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-import { RouterProvider } from "react-router";
+import { HashRouter, RouterProvider } from "react-router";
 import { router } from "./Routes/Routes";
 
-
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-    
-  </StrictMode>
+  <HashRouter>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </HashRouter>
 );
